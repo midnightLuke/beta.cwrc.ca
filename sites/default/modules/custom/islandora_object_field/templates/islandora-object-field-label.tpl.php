22199a88293eaddc90ae8e5818d21710ddb172b8
Luke Bainbridge <lbainbridge@echidna.ca>
2017-02-13 14:01:19 -0500

    Adding raw islandora_object_field

---
+++ sites/default/modules/custom/islandora_object_field/templates/islandora-object-field-label.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Displays an Islandora object field label.
+ *
+ * Available variables:
+ * - $label: The label text.
+ * - $pid: The ID of the object whose label is being displayed.
+ */
+echo l($label, 'islandora/object/' . $pid); ?>
