00ea70723a6cc892a6ef72b753a22c92cbab4f46
Luke Bainbridge <lbainbridge@echidna.ca>
2017-10-17 12:29:41 -0400

    Adding useful drush aliases.

---
+++ sites/all/drush/cwrc.aliases.drushrc.php
@@ -0,0 +1,29 @@
+<?php
+
+$aliases['dev-05'] = array(
+  'root' => '/var/www/html/drupal7',
+  'uri' => 'cwrc-dev-05.srv.ualberta.ca',
+  'remote-host' => 'cwrc-dev-05.srv.ualberta.ca',
+  'remote-user' => 'cwrcdesi',
+);
+
+$aliases['dev-06'] = array(
+  'root' => '/var/www/html/drupal7',
+  'uri' => 'cwrc-dev-06.srv.ualberta.ca',
+  'remote-host' => 'cwrc-dev-06.srv.ualberta.ca',
+  'remote-user' => 'cwrcdesi',
+);
+
+$aliases['dev-07'] = array(
+  'root' => '/var/www/html/drupal7',
+  'uri' => 'cwrc-dev-07.srv.ualberta.ca',
+  'remote-host' => 'cwrc-dev-07.srv.ualberta.ca',
+  'remote-user' => 'cwrcdesi',
+);
+
+$aliases['beta'] = array(
+  'root' => '/var/www/html/drupal7',
+  'uri' => 'beta.cwrc.ca',
+  'remote-host' => 'beta.cwrc.ca',
+  'remote-user' => 'cwrcdesi',
+);
